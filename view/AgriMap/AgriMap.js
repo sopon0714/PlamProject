066a98b4f7b78b10a07e1f685a4d9fff474a779b
SomNarawan <lovesom2112@hotmail.com>
2020-06-25 15:29:21 +0700

    map

--- view/AgriMap/AgriMap.js
+++ view/AgriMap/AgriMap.js
@@ -1,49 +1,139 @@
+function initMap(data) {
+    //The location of Uluru
+    if (data) {
 
-  function initMap() {
-    // The location of Uluru
-    var uluru = {
-        lat: 15.076367,
-        lng: 101.006522
-    };
-    // The map, centered at Uluru
-    var map = new google.maps.Map(
-        document.getElementById('map'), {
-            zoom: 4,
-            center: uluru
-        });
-    // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({
-        position: uluru,
-        map: map
-    });
+        var th = {
+            lat: 15.8700,
+            lng: 100.9925
+        };
+
+        var map = new google.maps.Map(
+            document.getElementById('map'), {
+                zoom: 4,
+                center: th
+            });
+
+        var location = JSON.parse(data);
+
+        var marker, i, info;
+
+        //console.log("dododod");
+
+
+
+        for (i = 0; i < location.length; i++) {
+            console.log(location[i].name, location[i].lat, location[i].lng);
+            marker = new google.maps.Marker({
+                position: new google.maps.LatLng(location[i].lat, location[i].lng),
+                title: location[i].name,
+                map: map
+            });
+
+            info = new google.maps.InfoWindow();
+            google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                return function() {
+                    info.setContent(location[i].name + "<br/>" + location[i].address + "<br/>" +
+                        "ปริมาณผลผลิต " + location[i].product + " กิโลกรัม" + "<br/>" +
+                        '<a href = "../OilPalmAreaVol/OilPalmAreaVol.php">เพิ่มเติม</a>');
+                    info.open(map, marker);
+                }
+            })(marker, i));
+
+        }
+
+    } else {
+        var th = {
+            lat: 15.8700,
+            lng: 100.9925
+        };
+        // The map, centered at Uluru
+        var map = new google.maps.Map(
+            document.getElementById('map'), {
+                zoom: 4,
+                center: th
+            });
+        // The marker, positioned at Uluru
+    }
+
+    //location.length=0;
 }
 
-/*$('#fertilizer_check').change(function(){
-    if($('#fertilizer_check').is(':checked'))
-    {
-        $("#fertilizer").prop('disabled', false);
+$("#check1").click(function() {
+    $("#product").attr("disabled", !this.checked);
+});
 
+$("#check2").click(function() {
+    $("#fertilizer").attr("disabled", !this.checked);
+});
+
+$("#check3").change(function() {
+    $("#water").data("ionRangeSlider").update({
+        "disable": !$(this).is(':checked')
+    });
+});
+
+$("#check4").change(function() {
+    $("#waterlack").data("ionRangeSlider").update({
+        "disable": !$(this).is(':checked')
+    });
+});
+
+$("#check5").change(function() {
+    $("#wash").data("ionRangeSlider").update({
+        "disable": !$(this).is(':checked')
+    });
+});
+
+$("#check6").click(function() {
+    $("#s_pesttype").attr("disabled", !this.checked);
+});
+
+
+$("#water").ionRangeSlider({
+    type: "double",
+    from: 1,
+    to: 1,
+    step: 1,
+    min: 0,
+    max: 365,
+    grid: true,
+    grid_num: 5,
+    grid_snap: false,
+    disable: true,
+    postfix: " วัน",
+    onFinish: function(data) {
+        console.log(data.to + " " + data.from);
     }
-    else
-    {
-        $("#fertilizer").prop('disabled', true);
-
+});
+$("#waterlack").ionRangeSlider({
+    type: "double",
+    from: 1,
+    to: 5,
+    step: 1,
+    min: 0,
+    max: 60,
+    grid: true,
+    grid_num: 10,
+    grid_snap: false,
+    disable: true,
+    postfix: " วัน",
+    onFinish: function(data) {
+        console.log(data.to + " " + data.from);
     }
-    
-
-})
-
-$('#product_check').change(function(){
-    if($('#product_check').is(':checked'))
-    {
-        $("#product").prop('disabled', false);
-
+});
+$("#wash").ionRangeSlider({
+    type: "double",
+    from: 1,
+    to: 5,
+    step: 1,
+    min: 0,
+    max: 30,
+    grid: true,
+    grid_num: 5,
+    grid_snap: false,
+    disable: true,
+    postfix: " ครั้ง",
+    onFinish: function(data) {
+        console.log(data.to + " " + data.from);
     }
-    else
-    {
-        $("#product").prop('disabled', true);
-
-    }
-    
-
-})*/
+});
--- view/AgriMap/AgriMap.php
+++ view/AgriMap/AgriMap.php
@@ -4,10 +4,11 @@ $idUT = $_SESSION[md5('typeid')];
 $CurrentMenu = "AgriMap";
 include_once("../layout/LayoutHeader.php");
 include_once("./../../query/query.php");
+$fpesttype = 0;
 
 $PROVINCE = getProvince();
 $YEAR = array();
-
+$PESTTYPE = getPestType();
 
 ?>
 
@@ -87,7 +88,7 @@ $YEAR = array();
                             </div>
                             <div class="row mb-3">
                                 <div class="col-12">
-                                    <select id="distrinct" class="form-control selectpicker" data-live-search="true"
+                                    <select id="distrinct" disabled class="form-control selectpicker" data-live-search="true"
                                         title="กรุณาเลือกอำเภอ">
                                         <option value="" hidden>เลือกทั้งหมด</option>
                                     </select>
@@ -189,11 +190,16 @@ $YEAR = array();
                             </div>
                             <div class="row mb-3">
                                 <div class="col-12">
-                                    <select id="pest" class="form-control" disabled>
-                                        <option value=0>ไม่พบ</option>
-                                        <option value=1 selected>ทั้งหมด</option>
-                                        <option value=2>แมลง</option>
-                                        <option value=3>โรคพืช</option>
+                                <select id="s_pesttype" name="s_pesttype" disabled class="form-control ">
+                                        <option selected value=0>เลือกทั้งหมด</option>
+                                        <?php
+                                            for ($i = 1; $i < sizeof($PESTTYPE); $i++) {
+                                                if ($fpesttype == $PESTTYPE[$i]["PTID"])
+                                                    echo '<option value="' . $PESTTYPE[$i]["PTID"] . '" selected>' . $PESTTYPE[$i]["TypeTH"] . '</option>';
+                                                else
+                                                    echo '<option value="' . $PESTTYPE[$i]["PTID"] . '">' . $PESTTYPE[$i]["TypeTH"] . '</option>';
+                                            }
+                                            ?>
                                     </select>
                                 </div>
                             </div>
@@ -215,7 +221,7 @@ $YEAR = array();
 <div class="container">
     <div class="card">
         <div class="card-header card-bg">
-            ตำแหน่งสวนปาล์ม
+            ตำแหน่งแปลงของสวนปาล์ม
         </div>
         <div class="card-body">
             <div class="col-12 mb-2">
@@ -233,190 +239,4 @@ $YEAR = array();
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBMLhtSzox02ZCq2p9IIuihhMv5WS2isyo&callback=initMap&language=th"
     async defer></script>
 
-
-<script>
-$(document).ready(function() {
-    $(document).on('change', '#province', function() {
-        $.ajax({
-            url: 'search_distrinct.php',
-            data: "province=" + $('#province').val(),
-            type: "POST",
-            async: false,
-            success: function(data) {
-                //alert(data);
-                $('#distrinct').html(data);
-                //$('#distrinct').selectpicker('refresh');
-            },
-            error: function(xhr, status, exception) {
-                alert(status);
-
-            }
-        });
-    });
-
-    $("#search").click(function() {
-        $.ajax({
-            url: 'search_map.php',
-            data: {
-                "distrinct": $('#distrinct').val(),
-                "fertilizer": $('#fertilizer').val(),
-
-                "product": $('#product').val(),
-                "year": $('#year').val()
-            },
-            type: "POST",
-            async: false,
-            success: function(data) {
-                //alert(data);
-                initMap(data);
-            },
-            error: function(xhr, status, exception) {
-                alert(status);
-            }
-        })
-    });
-
-});
-</script>
-
-<script>
-function initMap(data) {
-    //The location of Uluru
-    if (data) {
-
-        var th = {
-            lat: 15.8700,
-            lng: 100.9925
-        };
-
-        var map = new google.maps.Map(
-            document.getElementById('map'), {
-                zoom: 4,
-                center: th
-            });
-
-        var location = JSON.parse(data);
-
-        var marker, i, info;
-
-        //console.log("dododod");
-
-
-
-        for (i = 0; i < location.length; i++) {
-            console.log(location[i].name, location[i].lat, location[i].lng);
-            marker = new google.maps.Marker({
-                position: new google.maps.LatLng(location[i].lat, location[i].lng),
-                title: location[i].name,
-                map: map
-            });
-
-            info = new google.maps.InfoWindow();
-            google.maps.event.addListener(marker, 'click', (function(marker, i) {
-                return function() {
-                    info.setContent(location[i].name + "<br/>" + location[i].address + "<br/>" +
-                        "ปริมาณผลผลิต " + location[i].product + " กิโลกรัม" + "<br/>" +
-                        '<a href = "../OilPalmAreaVol/OilPalmAreaVol.php">เพิ่มเติม</a>');
-                    info.open(map, marker);
-                }
-            })(marker, i));
-
-        }
-
-    } else {
-        var th = {
-            lat: 15.8700,
-            lng: 100.9925
-        };
-        // The map, centered at Uluru
-        var map = new google.maps.Map(
-            document.getElementById('map'), {
-                zoom: 4,
-                center: th
-            });
-        // The marker, positioned at Uluru
-    }
-
-    //location.length=0;
-}
-
-$("#check1").click(function() {
-    $("#product").attr("disabled", !this.checked);
-});
-
-$("#check2").click(function() {
-    $("#fertilizer").attr("disabled", !this.checked);
-});
-
-$("#check3").change(function() {
-    $("#water").data("ionRangeSlider").update({
-        "disable": !$(this).is(':checked')
-    });
-});
-
-$("#check4").change(function() {
-    $("#waterlack").data("ionRangeSlider").update({
-        "disable": !$(this).is(':checked')
-    });
-});
-
-$("#check5").change(function() {
-    $("#wash").data("ionRangeSlider").update({
-        "disable": !$(this).is(':checked')
-    });
-});
-
-$("#check6").click(function() {
-    $("#pest").attr("disabled", !this.checked);
-});
-
-
-$("#water").ionRangeSlider({
-    type: "double",
-    from: 1,
-    to: 1,
-    step: 1,
-    min: 0,
-    max: 365,
-    grid: true,
-    grid_num: 5,
-    grid_snap: false,
-    disable: true,
-    postfix: " วัน",
-    onFinish: function(data) {
-        console.log(data.to + " " + data.from);
-    }
-});
-$("#waterlack").ionRangeSlider({
-    type: "double",
-    from: 1,
-    to: 5,
-    step: 1,
-    min: 0,
-    max: 60,
-    grid: true,
-    grid_num: 10,
-    grid_snap: false,
-    disable: true,
-    postfix: " วัน",
-    onFinish: function(data) {
-        console.log(data.to + " " + data.from);
-    }
-});
-$("#wash").ionRangeSlider({
-    type: "double",
-    from: 1,
-    to: 5,
-    step: 1,
-    min: 0,
-    max: 30,
-    grid: true,
-    grid_num: 5,
-    grid_snap: false,
-    disable: true,
-    postfix: " ครั้ง",
-    onFinish: function(data) {
-        console.log(data.to + " " + data.from);
-    }
-});
-</script>
+<script src="AgriMap.js"></script>
