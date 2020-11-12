c3159750c80078f71aa4abded83b2fcd0a06bfc6
SomNarawan <lovesom2112@hotmail.com>
2020-11-12 13:20:43 +0700

    chart -1 12/11/63

--- view/Chart/dataForChart.php
+++ view/Chart/dataForChart.php
@@ -251,7 +251,7 @@ if(isset($_POST['request'])){
             }else if($chose_type == "fertilize2"){
                 $sql = "SELECT t9.".$chose_label1." AS label1 ,".$label2." "."t9.`Type`,".$chose_cal."(t9.sumAll) AS data FROM (
                     SELECT t8.SF_dbID,t8.".$chose_label1.",t8.`Type`,ROUND(SUM( IF(t8.`Unit`=1,t8.`Vol`,t8.`Vol`/1000) ),2)AS sumAll FROM (
-                    SELECT `log-fertilisingdetail`.`Vol`,`log-fertilisingdetail`.`Unit`,`log-nutrient`.`Type`,t7.LID,t7.SF_dbID,t7.F_dbID,t7.F_name,t7.SF_name ,t7.SubDistrinct,t7.Distrinct,t7.Province,t7.dd,t7.Month,t7.Year2,t7.`dbID`AS FM_dbID,t8.FullName AS FM_name FROM (
+                    SELECT `log-fertilisingdetail`.`Vol`,`log-fertilisingdetail`.`Unit`,`log-nutrient`.`Type`,t7.LID,t7.SF_dbID,t7.F_dbID,t7.F_name,t7.SF_name ,t7.SubDistrinct,t7.Distrinct,t7.Province,t7.dd,t7.Month,t7.Year2,t7.`dbID`AS FM_dbID,t8f.FullName AS FM_name FROM (
                     SELECT t6.LID,t6.F_dbID,t6.F_name,t6.SF_dbID ,t6.SF_name ,t6.SubDistrinct,t6.Distrinct,t6.Province,t6.dd,t6.Month,t6.Year2,`dim-user`.`dbID` FROM (
                     SELECT t5.LID,t5.F_dbID,t5.F_name,t5.SF_dbID ,t5.SF_name,t5.SubDistrinct,t5.Distrinct,t5.Province,`dim-time`.dd,`dim-time`.Month,`dim-time`.Year2,t5.`DIMownerID` FROM (
                     SELECT t2.LID,t2.F_dbID,t2.F_name,t4.dbID AS SF_dbID,t4.Name AS SF_name,t2.SubDistrinct,t2.Distrinct,t2.Province,t2.`DIMdateID`,t2.`DIMownerID`  FROM (
@@ -282,8 +282,8 @@ if(isset($_POST['request'])){
                     JOIN `dim-user` ON `dim-user`.`ID` = `log-farmer`.`DIMuserID`
                     GROUP BY `dim-user`.`dbID`)AS FM
                     JOIN `log-farmer` ON `log-farmer`.`ID`= FM.max_lfID
-                    JOIN `dim-user` ON `dim-user`.`ID` = `log-farmer`.`DIMuserID`) AS t8
-                    ON t7.`dbID` = t8.`dbID`
+                    JOIN `dim-user` ON `dim-user`.`ID` = `log-farmer`.`DIMuserID`) AS t8f
+                    ON t7.`dbID` = t8f.`dbID`
                     JOIN `log-fertilisingdetail` ON `log-fertilisingdetail`.`fertilisingID` = t7.LID
                     JOIN `log-nutrient` ON `log-nutrient`.`ID` = `log-fertilisingdetail`.`logNID`
                     WHERE 1 ".$WHERE." )AS t8
